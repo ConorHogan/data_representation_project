9e65ea7b2264d28f775481473f179558f8f33dec
ConorHogan <cmfhogan@gmail.com>
2019-12-23 15:17:35 +0000

    window location origin fix

--- appconfig.py
+++ appconfig.py
@@ -1,8 +1,8 @@
 mysql = {
-  'host':"chogandatarep.mysql.pythonanywhere-services.com", 
-  'username':"chogandatarep", 
-  'password':"ch63107044", 
-  'database': "chogandatarep$datarepresentation" 
+  'host':"chogandatarep.mysql.pythonanywhere-services.com",
+  'username':"chogandatarep",
+  'password':"ch63107044",
+  'database': "chogandatarep$datarepresentation"
 }
 
 auth = {'username':"admin", 'password':"letmein123"}
